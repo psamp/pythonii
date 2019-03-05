45974de3d16d5305867e0da06955943c87b05bac
princess sampson <psampson@princesss-mbp.spelman.edu>
2019-03-05 13:41:49 -0500

    added comments to bank demo, created dict review exercises

--- bank_demo.py
+++ bank_demo.py
@@ -1,16 +1,22 @@
 from bank import BankAccount
 import uuid
 
+# create account
 def createAccount():
+    # get information from user and generate an id
     owner = input('Enter your name: ')
     balance = float(input('Enter your initial deposit: '))
     id = uuid.uuid4().hex
 
+    # return the new bank account object
     return BankAccount(owner, balance, id)
 
+# given an account, ask a user to choose a menu option and print appropriate info
 def running(account):
+    # user input
     choice = input("1. Deposit\n2. Withdraw\n3. Check\n")
-
+    
+    # print info based on user choice
     if choice == '1':
         amount = float(input('How much would you like to deposit?\n'))
         account.deposit(amount)
@@ -22,13 +28,21 @@ def running(account):
 
 
 def main():
+    # create an account
     account = createAccount()
+
+    # game loop flag
     playing = True
 
+    # while the user wants to play
     while playing:
+        # delegate to menu/info function
         running(account)
+
+        # ask the user if they would like to quit
         quit = input('Quit? Y/N')
 
+        # set the flag to false if they want to quit
         playing = True if quit != 'Y' else False
 
 # call main
---
+++ dictionary_review.py
@@ -0,0 +1,16 @@
+def make_dictionary(keys, values):
+
+    if len(keys) != len(values):
+        raise Exception('Must have equal number of keys and values')
+
+    dictionary = dict()
+
+    
+    for index in range(len(keys))
+
+def main():
+    names = ['Ahsoka', 'Padme', 'Anakin', 'Obi-Wan']
+    scores = [100, 80, 90, 85, 75]
+
+# call main
+main()
