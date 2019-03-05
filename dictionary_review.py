b302b5589e4757bc5eb13401890e7ccbc8be97e5
princess sampson <psampson@princesss-mbp.spelman.edu>
2019-03-05 13:57:52 -0500

    completed part ii exercises

--- dictionary_review.py
+++ dictionary_review.py
@@ -1,16 +1,52 @@
-def make_dictionary(keys, values):
+import statistics
 
+def make_dictionary(keys, values):
+    # throw an exception if not same no. of keys as values
     if len(keys) != len(values):
         raise Exception('Must have equal number of keys and values')
 
     dictionary = dict()
 
+    # loop over both lists
+    for index in range(len(keys)):
+        # pull out corresponding keys and values
+        key = keys[index]
+        value = values[index]
+
+        # for the key, insert the value into the dict
+        dictionary[key] = value
     
-    for index in range(len(keys))
+    return dictionary
 
 def main():
-    names = ['Ahsoka', 'Padme', 'Anakin', 'Obi-Wan']
-    scores = [100, 80, 90, 85, 75]
+    names = ['Tonya', 'Mary', 'Alexis', 'Angel']
+    scores = [85, 75, 90, 80]
+
+    # create dict from lists
+    score_dict = make_dictionary(names, scores)
+    
+    # print alexis
+    print(score_dict['Alexis'])
+
+    # add joshua
+    score_dict['Joshua'] = 95
+
+    # sorted list of the scores
+    scores_sorted = sorted(score_dict.values())
+    print(scores_sorted)
+
+    # avg score
+    avg_score = statistics.mean(list(score_dict.values()))
+    print(avg_score)
+
+    # remove mary
+    score_dict.pop('Mary')
+    print(score_dict)
+
+    # print table sorted by name
+    for name, score in sorted(score_dict.items()):
+        print('%s\t%d' % (name, score))
+
 
 # call main
 main()
